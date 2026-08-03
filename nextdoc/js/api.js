2b629f0ea2a13e146e3c5ddfbe88f6889e26f858
Ismael Silva <meloismael827@gmail.com>
2026-08-03 20:23:45 -0300

    Atualização 2.0

--- nextdoc/js/api.js
+++ nextdoc/js/api.js
@@ -5,7 +5,7 @@
 // o app continua funcionando 100% com o IndexedDB (modo simples).
 
 const SecureAPI = (function () {
-  const BASE_URL = "https://nextdoc-api-xxxx.onrender.com/api";
+  const BASE_URL = "https://nexdoc-api.onrender.com";
   let available = null; // null = ainda não checado
 
   function withTimeout(promise, ms) {
